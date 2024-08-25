a0877129a7694280ae79b6b2399ffebc9bb5e245
Barbara Machado <147945840+BarbarizandoDev@users.noreply.github.com>
2024-08-25 19:21:24 +0100

    Implementação da listagem de produtos na listagemVIEW

--- LeiloesTDSat/src/listagemVIEW.java
+++ LeiloesTDSat/src/listagemVIEW.java
@@ -1,4 +1,5 @@
 
+import java.awt.List;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
 
@@ -138,8 +139,7 @@ public class listagemVIEW extends javax.swing.JFrame {
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
         String id = id_produto_venda.getText();
         
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
+        ProdutosDAO dao = new ProdutosDAO();
         //produtosdao.venderProduto(Integer.parseInt(id));
         listarProdutos();
     }//GEN-LAST:event_btnVenderActionPerformed
